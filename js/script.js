dd438805ebe5cf875a140ee7f1762323a51fed56
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-01 15:59:58 +0200

    Remove img from HTML and add by JS

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@ const galleryElement = document.getElementById("gallery");
 const btnPrev = document.getElementById("btn-prev");
 const btnNext = document.getElementById("btn-next");
 
+//* Creo l'array con il percorso delle immagini
+const immages = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp",
+];
+
+//# Variabili
+let imgElement;
+
+// //# Creo il ciclo per inserire le immagini
+for (let i = 0; i < immages.length; i++) {
+  imgElement = document.createElement("img");
+  imgElement.src = immages[i];
+  imgElement.alt = "Landscape " + (i + 1);
+  galleryElement.appendChild(imgElement);
+}
+
 //# LOGICA
 const images = document.querySelectorAll("#carousel img");
 let currentIndex = 0;
